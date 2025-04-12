399b8b283aaa38d7620e85dcb25d9a9b4d92b456
sidhrashid <sidhpurarashid298@gmail.com>
2025-04-12 11:36:31 +0530

    upload by sr

--- client/src/components/clientCompo/Searchbar.jsx
+++ client/src/components/clientCompo/Searchbar.jsx
@@ -1,37 +1,37 @@
-// import { FaSearch } from "react-icons/fa";
-// import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { useSearchCart } from "../../context/SearchContext";
+import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSearchCart } from "../../context/SearchContext";
 
-// function Searchbar() {
-//   const { setSearchQuery } = useSearchCart();
-//   const [query, setQuery] = useState("");
-//   const navigate = useNavigate();
+function Searchbar() {
+  const { setSearchQuery } = useSearchCart();
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
-//   const handleSearch = (e) => {
-//     e.preventDefault();
-//     const trimmedQuery = query.trim();
-//     setSearchQuery(trimmedQuery);
-//     navigate("/allproducts");
-//   };
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const trimmedQuery = query.trim();
+    setSearchQuery(trimmedQuery);
+    navigate("/allproducts");
+  };
 
-//   return (
-//     <form
-//       onSubmit={handleSearch}
-//       className="hidden sm:hidden md:flex items-center justify-between gap-4 bg-gray-100 p-2 rounded-md w-full sm:w-[200px] md:w-[300px] lg:w-[400px] xl:w-[500px] 2xl:w-[600px]"
-//     >
-//       <input
-//         type="text"
-//         placeholder="Search"
-//         value={query}
-//         onChange={(e) => setQuery(e.target.value)}
-//         className="bg-transparent outline-none w-full"
-//       />
-//       <button type="submit" className="cursor-pointer p-1 hover:bg-gray-200 rounded-md">
-//         <FaSearch className="text-gray-600" />
-//       </button>
-//     </form>
-//   );
-// }
+  return (
+    <form
+      onSubmit={handleSearch}
+      className="hidden sm:hidden md:flex items-center justify-between gap-4 bg-gray-100 p-2 rounded-md w-full sm:w-[200px] md:w-[300px] lg:w-[400px] xl:w-[500px] 2xl:w-[600px]"
+    >
+      <input
+        type="text"
+        placeholder="Search"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="bg-transparent outline-none w-full"
+      />
+      <button type="submit" className="cursor-pointer p-1 hover:bg-gray-200 rounded-md">
+        <FaSearch className="text-gray-600" />
+      </button>
+    </form>
+  );
+}
 
-// export default Searchbar;
+export default Searchbar;
