14c789a72c46667fc6726bbeead99e672d6e96e2
sidhrashid <sidhpurarashid298@gmail.com>
2025-04-12 18:11:25 +0530

    upload by sr

--- server/index.js
+++ server/index.js
@@ -12,10 +12,18 @@ const adminUser = require("./routes/loginRoute/AdminLoginRoute");
 const cartItem = require("./routes/pagesRoute/CartItemRoutes");
 const paymentRoutes = require("./routes/paymentMethodRoute/PaymentRoute");
 
-
 const app = express();
 const PORT = process.env.PORT;
 const URL = process.env.URL;
+
+app.use(
+  cors({
+    origin: "https://e-com-web-1-srky.onrender.com", // Frontend ka URL yaha daalein
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed methods
+    credentials: true, // Agar aap cookies pass karna chahte hain
+    allowedHeaders: "Content-Type, Authorization", // Allowed headers
+  })
+);
 
 app.use(express.json());
 app.use(bodyparser.json());
@@ -26,18 +34,8 @@ app.use("/", products);
 app.use("/", category);
 app.use("/", adminUser);
 app.use("/", cartItem);
-app.use('/', paymentRoutes);
-
-app.use(
-  cors({
-    origin: "https://e-com-web-1-srky.onrender.com",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-    allowedHeaders: "Content-Type, Authorization",
-  })
-);
-
+app.use("/", paymentRoutes);
 
 app.listen(PORT, () => {
-  console.log(`server is running on port ${URL}`);
+  console.log(`Server is running on port ${URL}`);
 });
